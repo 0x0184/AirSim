a2316ee3874bd2c7e78c755cc135e2a9158f8899
rapsealk <piono623@naver.com>
2019-05-23 18:48:27 +0900

    Add: AirSim RpcLibPort header

---
+++ AirLib/include/api/RpcLibPort.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef air_RpcLibPort_hpp
+#define air_RpcLibPort_hpp
+
+const uint16_t RpcLibPort = 39311;
+
+#endif
